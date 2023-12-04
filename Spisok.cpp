bd1c5a918fb3b33c1fbbe232598dfafeb2332d19
den3yb <126419855+den3yb@users.noreply.github.com>
2023-12-04 15:40:48 +0400

    visual studia crash

--- Spisok.cpp
+++ Spisok.cpp
@@ -17,15 +17,15 @@ namespace spisok {
 		note() { _next = nullptr; _prev = nullptr; _val = nullptr; }
 		note(T data) { _next = nullptr; _prev = nullptr; _val = new T(data); }
 		note(note* temp1, note* temp2, T data) { _next = temp1; _prev = temp2; _val = new T(data); }
-		note(note<T>& another){ _next = another._next; _prev = another._prev; _val = new T(*another._val); }
+		note(note<T>& another) { _next = another._next; _prev = another._prev; _val = new T(*another._val); }
 		~note() { _next = nullptr; _prev = nullptr; delete _val; };
 		note* get_n() { return _next; }
 		note* get_p() { return _prev; }
 		T* get_v() { return _val; }
 		void set_n(note* next) { _next = next; }
 		void set_p(note* prev) { _prev = prev; }
-		void set_v(T* val) {_val = val; }
-		friend ostream& operator<< (ostream& o, note<T>& t) { cout << *(t.get_v())<<" "; return o; }
+		void set_v(T* val) { _val = val; }
+		friend ostream& operator<< (ostream& o, note<T>& t) { cout << *(t.get_v()) << " "; return o; }
 		friend bool operator== (note<T> rhs, note<T> lhs) {
 			if (*(rhs.get_v()) == *(lhs.get_v())) { return false; }
 			else { return true; }
@@ -34,8 +34,8 @@ namespace spisok {
 			if (*(rhs.get_v()) == *(lhs.get_v())) { return true; }
 			else { return false; }
 		}
-		void operator= (const note<T> lhs) { _next = lhs._next; _prev = lhs._prev; _val = lhs._val;}
-		
+		void operator= (const note<T> lhs) { _next = lhs._next; _prev = lhs._prev; _val = lhs._val; }
+
 	};
 
 	template <typename T>
@@ -45,7 +45,7 @@ namespace spisok {
 		note<T>* _end;
 	public:
 
-		twolist() { _head = nullptr; _end=nullptr; }
+		twolist() { _head = nullptr; _end = nullptr; }
 
 		twolist(const twolist& another) {
 			_head = nullptr;
@@ -60,13 +60,14 @@ namespace spisok {
 			}
 		}
 
-		twolist(int size) {
-			_head = nullptr;
-			_end = nullptr;
-			for (int i = 0; i < size; size++){this->push_end(new note<T>(new T(random())));}
+		twolist(int size, T min, T max) {
+			_head, _end = nullptr;
+			for (int i = 0; i < size; i++) {
+				this->push_end(new note<T>(random(min, max)));
+			}
 		}
 
-		T random() {
+		T random(T min, T max) {
 			std::random_device random_device;
 			std::mt19937 generator(random_device());
 			std::uniform_int_distribution<> distribution(min, max);
@@ -81,11 +82,11 @@ namespace spisok {
 				start->set_n(nullptr);
 				start->set_p(nullptr);
 			}
-			else{
+			else {
 				start->set_p(nullptr);
 				start->set_n(_head);
 				_head->set_p(start);
-				_head = start; 
+				_head = start;
 			}
 		}
 
@@ -132,16 +133,16 @@ namespace spisok {
 			int len = this->len();
 			for (int i = 0; i < len; i++) {
 				if (*(temp->get_v()) == val) {
-					if (temp == _head){this->pop_head();}
+					if (temp == _head) { this->pop_head(); }
 					else {
-						if (temp == _end){ this->pop_end(); }
+						if (temp == _end) { this->pop_end(); }
 						else
 						{
 							temp->get_p()->set_n(temp->get_n());
 							temp->get_n()->set_p(temp->get_p());
 						}
 					}
-					
+
 				}
 				temp = temp->get_n();
 			}
@@ -152,7 +153,7 @@ namespace spisok {
 		friend ostream& operator<< (ostream& out, twolist<T>& list) {
 			note<T>* start = list._head;
 			while (start != nullptr) {
-				cout << *start ;
+				cout << *start;
 				start = start->get_n();
 			}
 			return out;
@@ -160,9 +161,9 @@ namespace spisok {
 
 		note<T>* operator[](int index) {
 			note<T>* start = _head;
-			if (index>this->len()) { throw runtime_error("Index is out of range"); }
+			if (index > this->len()) { throw runtime_error("Index is out of range"); }
 			if (start == nullptr && index == 0) { return _head; }
-			for (int i = 0; i < index; i++) {start = start->get_n();}
+			for (int i = 0; i < index; i++) { start = start->get_n(); }
 			return start;
 		}
 
@@ -175,10 +176,24 @@ namespace spisok {
 				len += 1;
 				temp = temp->get_n();
 			}
-			return len+1;
+			return len + 1;
 		}
 
+		note<T>* get_end() { return _end; }
 	};
 
+	void sum_num(twolist<int>* lhs, twolist<int>* rhs) {
+		note<int>* left = lhs->get_end();
+		note<int>* right = rhs->get_end();
+		int overf = 0;
+		while (left != nullptr && right != nullptr) {
+			int a = *(left->get_v()) + *(right->get_v());
+			cout << a;
+			left->set_v(&a);
+			left = left->get_p();
+			right = right->get_p();
+		}
+		
+	}
 
 }
--- Tests.cc
+++ Tests.cc
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace spisok;
 
-TEST(Func_Tests, SumStubTest1) {
+TEST(Tests1, twolist1) {
 	note<int> a (nullptr, nullptr, 7);
 	note<int> b(nullptr, nullptr, 2);
 	note<int> c(nullptr, nullptr, 5);
@@ -24,7 +24,24 @@ TEST(Func_Tests, SumStubTest1) {
 	list1.deleate(2);
 	cout << list1<<"\n";
 	int i = 4;
-	twolist<int> list3(i);
-EXPECT_TRUE(true);
+	twolist<int> list3(i, 1, 10);
+	cout << list3;
+	EXPECT_TRUE(true);
 }
 
+TEST(Tests2, summ) {
+	note<int> a(nullptr, nullptr, 4);
+	note<int> b(nullptr, nullptr, 2);
+	note<int> c(nullptr, nullptr, 5);
+	twolist<int> list1;
+	list1.push_end(&a);
+	list1.push_end(&b);
+	list1.push_end(&c);
+	twolist<int> list2(list1);
+	cout << list1 << "\n";
+	cout << list2 << "\n";
+	sum_num(&list1, &list2);
+	cout << list1 << "\n";
+	cout << list2 << "\n";
+	EXPECT_TRUE(true);
+}
